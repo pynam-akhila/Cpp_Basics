481fa433ab539da944b283debb75550f9e15ab30
Akhila Pynam <pynam.akhila@gmail.com>
2026-09-25 22:13:08 +0530

    Update sum-of-2-numbers.cpp

--- sum-of-2-numbers.cpp
+++ sum-of-2-numbers.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h>
+using namespace  std;
 
+// DATA TYPES : int - integers , char - characters, float - decimal,
+//              double - decimal, string - words
+
+// How to add 2 numbers / sum of 2 numbers 
+// you need to have variables
+
+int main(){
+
+    // case 1 : integers 
+    
+    int x = 5;
+    int y = 2;
+
+    int sum = x + y;
+
+    cout << sum << endl; // doing sum ( integer - no " ")
+
+    cout << "Akhila Pynam" << endl; // my name as output ( string - " ")
+
+    // case 2 : 
+
+    double m = 7.6;
+    double n = 5.2;
+    
+    double s = m + n;
+
+    cout << s << endl;
+
+    return 0;
+
+}
